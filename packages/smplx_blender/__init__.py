f83ddf98de5b103dfec42e49eebc30e748247fd6
FigoHunter <zhaoyifei19950308@hotmail.com>
2023-09-18 12:28:40 +0800

    delete

--- packages/smplx_blender/__init__.py
+++
@@ -1,4 +0,0 @@
-package_name = "smplx_blender"
-version = (0, 1, 0)
-
-from . import ops
--- packages/smplx_blender/bobject.py
+++
@@ -1,4 +0,0 @@
-import bpy
-
-def removeObject(object_to_delete):
-    bpy.data.objects.remove(object_to_delete, do_unlink=True)
